9eea609e5afb158b866c8248acad412eb39411ab
Kris Papercut <krispcut@gmail.com>
2024-10-11 19:43:26 +0300

    v1.0.7

--- src/volumeParse.ts
+++ src/volumeParse.ts
@@ -1,7 +1,7 @@
 import {Volume} from "./volumeFormat";
 
 
-const volumeParse = (volume: string): Volume => {
+export const volumeParse = (volume: string): Volume => {
     const regVolume = /^([^:]+):([^:]+)(?::([^:]+))?$/;
 
     const [, source, destination, options] = regVolume.exec(volume) || [];
